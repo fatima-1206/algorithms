8d28a35b9d97f6d6ff05dc3a6e922e55697c5e5d
Fatima Tu Zahra <156695063+fatima-1206@users.noreply.github.com>
2025-12-14 03:22:40 +0500

    max value gifts

--- 3_max_value_of_gifts.cpp
+++ 3_max_value_of_gifts.cpp
@@ -19,35 +19,49 @@ using namespace std;
 
 int maxGifts(int n, int m, vector<vector<int>> gifts) {
     //vector to store results
-    vector<vector<int> > dp(n+1,vector<int>(m+1, 0));
+    vector<vector<int> > dp(n, vector<int>(m, 0));
     //dp[i][j]=maximum attainable value of gifts for the matrix of rows i and columns j only
-    for(int i=1;i<=n;i++) {
-        for(int j=1;j<=m;j++) {
-            //There are two ways to reach colums (i,j)
+    
+    dp[0][0] = gifts[0][0];
+    
+    // Fill first row
+    for(int j = 1; j < m; j++) {
+        dp[0][j] = dp[0][j-1] + gifts[0][j];
+    }
+    
+    // Fill first column
+    for(int i = 1; i < n; i++) {
+        dp[i][0] = dp[i-1][0] + gifts[i][0];
+    }
+    
+    // Fill rest of the table
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j < m; j++) {
+            //There are two ways to reach (i,j)
             //one from above and one from left
             //choose the one which gives more value 
-            dp[i][j]=max(dp[i][j-1],dp[i-1][j])+gifts[i][j];
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j]) + gifts[i][j];
         }
     }
-    // make it print the dp
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= m; j++) {
+    
+    // print the dp
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cout << dp[i][j] << " ";
         }
         cout << endl;
     }
-    return dp[n][m];
+    return dp[n-1][m-1];
 }
 
 int main()
 {
     // sample
     vector<vector<int> > gifts1 = {
-        {0, 0, 0, 0, 0},
-        {0, 1, 10, 3, 8},
-        {0, 12, 2, 9, 6},
-        {0, 5, 7, 4, 11},
-        {0, 3, 7, 16, 5}
+        {1, 10, 3, 8},
+        {12, 2, 9, 6},
+        {5, 7, 4, 11},
+        {3, 7, 16, 5}
     };
     int n = 4; // number of rows
     int m = 4; // number of columns
@@ -57,10 +71,10 @@ int main()
 
     cout<<"Enter the number of rows and columns"<<endl;
     cin>>n>>m;
-    vector<vector<int> > gifts(n+1,vector<int>(m+1));
+    vector<vector<int> > gifts(n, vector<int>(m));
     cout<<"Enter the values of gifts in cells"<<endl;
-    for(int i=1;i<=n;i++)
-        for(int j=1;j<=m;j++)
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
             cin>>gifts[i][j];
 
     cout<<"maximum attainable value of gifts is "<<endl;
