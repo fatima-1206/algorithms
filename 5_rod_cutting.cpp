97865e5b8fb50adbbaebec680d08ca5f1c4a9c32
Fatima Tu Zahra <156695063+fatima-1206@users.noreply.github.com>
2025-12-16 16:25:42 +0500

    task 5

--- 5_rod_cutting.cpp
+++ 5_rod_cutting.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 using namespace std;
 
-int cutRodRecur(int n, vector<int> &price) {
+int cutRod(int n, vector<int> &price) {
     
     // Base case
     if (n==0) return 0;
@@ -17,19 +17,15 @@ int cutRodRecur(int n, vector<int> &price) {
     // recursively find value of rod of
     // length (n-j).
     for (int i=1; i<=n; i++) 
-        ans = max(ans, price[i-1]+cutRodRecur(n-i, price));
+        ans = max(ans, price[i-1]+cutRod(n-i, price));
     
     return ans;
 }
 
-int cutRod(vector<int> &price) {
-    int n = price.size();
-    
-    return cutRodRecur(n, price);
-}
+
 
 int main() {
     vector<int> price =  { 1, 5, 8, 9, 10, 17, 17, 20};
-    cout << cutRod(price) << '\n';
+    cout << cutRod(price.size(), price) << '\n';
     return 0;
 }
